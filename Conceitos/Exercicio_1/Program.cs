0b47ad5c73de8fbf79974781c3c575cd45787b8d
gutoFaria <61546216+gutoFaria@users.noreply.github.com>
2021-10-23 13:13:00 -0300

    adicionando exercicios 1 e 2

---
+++ Conceitos/Exercicio_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Console.Clear();// Iniciando o programa limpando o terminal
+            Console.WriteLine("CALCULO DA MÉDIA PONDERADA 3 DE NOTAS ");
+
+            //PEGANDO AS NOTAS E PESOS
+            Console.Write("Digite a primeira nota do aluno:");
+            float a1 = float.Parse(Console.ReadLine());
+            Console.Write("Digite o peso da primeira nota:");
+            float p1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota do aluno:");
+            float a2 = float.Parse(Console.ReadLine());
+            Console.Write("Digite o peso da segunda nota:");
+            float p2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a terceira nota do aluno:");
+            float a3 = float.Parse(Console.ReadLine());
+            Console.Write("Digite o peso da terceira nota:");
+            float p3 = float.Parse(Console.ReadLine());
+
+           //calculo da média ponderada
+            float mediaPonderada = ((a1 * p1) + (a2 * p2) + (a3 * p3))/(p1+p2+p3);
+            Console.WriteLine("");
+            //utilizando o comando Math.Round para exibir apenas duas casas decimais
+            Console.WriteLine("A média ponderada das 3 notas é: " + Math.Round(mediaPonderada,2));  
+        }
+    }
+}
---
+++ Conceitos/Exercicio_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha = 0;
+            float n = 0;
+            float p = 0;
+            Console.Write("Digite o número de notas que será Calculado a média:");
+            int numeroNotas = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.Write("Digite -1 para sair ou 0 para continuar:");
+                escolha = int.Parse(Console.ReadLine());
+                if(escolha == -1)
+                    break;
+                Console.Write("Digite sua nota: ");
+                n = float.Parse(Console.ReadLine());
+                Console.Write("Digite o peso da nota: ");
+                p = float.Parse(Console.ReadLine()); 
+
+            } while (escolha != -1);
+
+           
+        }
+    }
+}
